eca602271a0ae67d150bcfb0d01860169f090f2d
dwiki <dwiki.nuridhuha@gmail.com>
2022-04-12 20:56:47 +0700

    bwa - 7 - cache cdn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import {useEffect, useState} from 'react';
 import {Header, Hero, Browse, Arrived, Client, AsideMenu, Footer} from './components';
 
 function App() {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept': 'application/json',
+                    'x-api-key': process.env.REACT_APP_APIKEY
+                }
+            });
+
+            const {nodes} = await response.json();
+            setItems(nodes);
+        };
+
+        getProducts();
+    }, []);
+
     return (
         <>
             <Header/>
             <Hero/>
             <Browse/>
-            <Arrived/>
+            <Arrived items={items}/>
             <Client/>
             <AsideMenu/>
             <Footer/>
